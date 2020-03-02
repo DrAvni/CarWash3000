07cb328627d735cbfe52e50b67b904fd71fb8dbd
Avni Rexhaj <AvniRexhaj@hotmail.com>
2020-03-02 14:16:51 -0500

    Initial Commit

---
+++ CarWash3000/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarWash3000
+//
+//  Created by Avni Rexhaj on 3/2/20.
+//  Copyright © 2020 Avni Rexhaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CarWash3000Tests/CarWash3000Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CarWash3000Tests.swift
+//  CarWash3000Tests
+//
+//  Created by Avni Rexhaj on 3/2/20.
+//  Copyright © 2020 Avni Rexhaj. All rights reserved.
+//
+
+import XCTest
+@testable import CarWash3000
+
+class CarWash3000Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
